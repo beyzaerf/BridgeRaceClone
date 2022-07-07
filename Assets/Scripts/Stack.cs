8def801baae0ef3becbe9d4c82045431c8d2ff59
beyzaerf <beyzaerfidan@gmail.com>
2022-07-07 14:42:54 +0300

    Bug fix: collider not getting destroyed

--- Assets/Scripts/Stack.cs
+++ Assets/Scripts/Stack.cs
@@ -9,10 +9,12 @@ public class Stack : MonoBehaviour
     [SerializeField] private GameObject stackObject;
     [SerializeField] private List<GameObject> bricks;
     [SerializeField] private GameObject colliderPrefab;
+    public GameObject collObject;
 
     private void Start()
     {
         prevObject = stackObject.transform.GetChild(0).gameObject; //Initially, previous object is set to an empty object that is the child of stackObject
+        collObject = new GameObject();
     }
     private void OnTriggerEnter(Collider other)
     {
@@ -35,13 +37,12 @@ public class Stack : MonoBehaviour
         //Leaving bricks
         else if (other.transform.tag.StartsWith("Bridge") && !other.transform.CompareTag("BridgeP"))
         { // If the object we touch's tag starts with Bridge but doesnt equal BridgeP (meaning that we already activated it)
-            //GameObject collObject = null;
-            if(bricks.Count > 1)
+            if (bricks.Count > 1)
             {
                 GameObject myObject = bricks[bricks.Count - 1];
                 bricks.RemoveAt(bricks.Count - 1);
                 Destroy(myObject);
-
+                    
                 other.GetComponent<MeshRenderer>().material = transform.GetChild(1).GetComponent<SkinnedMeshRenderer>().material; //Changing the material of the bridge to our color
                 other.GetComponent<MeshRenderer>().enabled = true; //Enabling the bricks on the bridge
                 other.GetComponent<BoxCollider>().isTrigger = false;
@@ -49,13 +50,35 @@ public class Stack : MonoBehaviour
                 other.tag = "BridgeP"; //Changing the tag after leaving bricks
             }
             else if (bricks.Count <= 1) //If the bricks the player is carrying ends
-            {
+            { 
                 Vector3 playersPos = new(transform.position.x, transform.position.y, transform.position.z + 0.2f);
-                Instantiate(colliderPrefab, playersPos, Quaternion.Euler(0, 0, 0));
+                if (collObject != null)
+                {
+                    GameObject ifObject = Instantiate(colliderPrefab, playersPos, Quaternion.Euler(0, 0, 0)) as GameObject;
+                    ifObject.transform.parent = collObject.transform;
+                }
+                else
+                    collObject = new GameObject();
             }
-            //Destroy(collObject);
             prevObject = bricks[0];
+            if (collObject! && collObject.transform.childCount > 0)
+            {
+                collObject.tag = "Bridge";
+                collObject.transform.GetChild(0).tag = "Bridge";
+            }
         }
     }
-    
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.CompareTag("Bridge"))
+        {
+            if(bricks.Count > 1)
+            {
+                if (collObject.transform.childCount > 0)
+                {
+                    Destroy(collObject);
+                }
+            }
+        }
+    }
 }
